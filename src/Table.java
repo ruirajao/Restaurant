b274912077278d0ae09888194a7962109ea9c3f4
DaniSpitzer <108369821+DaniSpitzer@users.noreply.github.com>
2023-01-10 16:49:09 +0000

    Propriedades e métodos criados.

--- src/Table.java
+++ src/Table.java
@@ -1,2 +1,26 @@
 public class Table {
+
+    public boolean free;
+    public String order;
+
+
+    public void cleanOrder(){
+
+    }
+    public void getOrder(){
+
+    }
+    public void isFree(){
+
+    }
+    public void occupy(){
+
+    }
+    public void saveOrder(){
+
+    }
+    public void unOccupy(){
+
+    }
+
 }
