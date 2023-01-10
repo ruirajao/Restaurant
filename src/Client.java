7b5eaf151c21ec962f46413eb80704bdf3821881
Miguel Aguiar <aguiar425@gmail.com>
2023-01-10 16:46:25 +0000

    teste

--- src/Client.java
+++ src/Client.java
@@ -1,2 +1,23 @@
 public class Client {
+
+    private int tableNumber;
+
+    public void askForTable() {
+
+    }
+
+    public void order() {
+
+    }
+
+    public void pay() {
+
+    }
+
+    public void setRestaurant() {
+
+    }
+
+    private void checkRestaurantPresence() {
+    }
 }
