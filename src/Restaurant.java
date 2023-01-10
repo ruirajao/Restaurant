b410935dcba2ba66b2ce0659dc506de5fa0a3e5c
ruirajao <VdPSTH5Cz@Scfx3>
2023-01-10 16:48:37 +0000

    First commit

--- src/Restaurant.java
+++ src/Restaurant.java
@@ -1,2 +1,37 @@
 public class Restaurant {
+    public String c [] Client = new Client[];
+    public Boolean fullCapacity;
+    public int numberofOccupiedTables;
+
+    public static int restaurantCount;
+
+    public int tables [] Table = new Table[];
+
+    public void Restaurant() {
+
+    }
+
+    public void findTable() {
+
+    }
+
+    public void freeTable() {
+
+    }
+
+    public static int getRestaurantCount() {
+        return restaurantCount;
+    }
+
+    public static void receiveOrder() {
+        
+    }
+    public static void checkIfTableCanBeFreed() {
+
+    }
+
+    public static void  occupyTable() {
+
+    }
+
 }
